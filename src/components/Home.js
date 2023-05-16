6d64e1c8b562fd5078bd28ef12ce06241f8f666c
Ahmed Matter <matterahmed36@gmail.com>
2023-05-16 03:44:50 +0300

    test 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Draggable from 'react-draggable';
-import { motion } from 'framer-motion';
+import { motion, steps } from 'framer-motion';
+import './LandingPage.css';
 function Home() {
   const [image, setImage] = useState(null);
 
@@ -83,7 +84,7 @@ function Home() {
             ref={el => (divRef.current[piece.id] = el)}
             id={piece.id}
             style={{
-              border: '1px solid black',
+              border: '1px solid white',
               width: '100px',
               height: '100px',
             }}
@@ -128,102 +129,142 @@ function Home() {
         flexDirection: 'column',
         position: 'relative',
       }}
+      className='contaner'
     >
-      <h1> Matter Puzzle Game </h1>{' '}
-      <input type='file' onChange={handleImageUpload} /> <br />{' '}
       {end ? (
-        <div>
-          <h1> You Win </h1>{' '}
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '20px',
+            color: 'white',
+          }}
+        >
+          {/* {(document.getElementById('main').style.display = 'flex')} */}
+          <h1> Congrtiolations you finsh it in {stebs} step </h1>
+          <img
+            src={URL.createObjectURL(image)}
+            alt='S'
+            id='main'
+            style={{ display: 'flex', width: '400px', height: '600px' }}
+          />
         </div>
       ) : (
-        image && (
-          <>
-            <img
-              src={URL.createObjectURL(image)}
-              alt='S'
-              onLoad={handleImageLoad}
-              id='main'
-            />{' '}
-            <br />
-            <label htmlFor='pieces'> Number of Pieces: </label>{' '}
-            <select id='pieces' value={pieces} onChange={handlePiecesChange}>
-              <option value={2}> 4 </option> <option value={4}> 16 </option>{' '}
-              <option value={9}> 81 </option>{' '}
-            </select>{' '}
-            <br /> <>{stebs}</>
-            <div
-              style={{
-                display: 'flex',
-                width: '50%',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'row-reverse',
-                // position: 'relative',
-              }}
-            >
-              {rows.map(row => (
-                <div
-                  style={{
-                    display: 'flex',
-                    width: '50%',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    flexDirection: 'row-reverse',
-                    // position: 'relative',
-                  }}
-                >
-                  {row.map((piece, index) => (
-                    <img
-                      ref={el => (imgRef.current[piece.id] = el)}
-                      style={{
-                        position: 'realative',
-                      }}
-                      alt={`Piece ${piece.id}`}
-                      key={piece.id}
-                      id={piece.id}
-                      src={piece.dataUrl}
-                      onDragStart={(event, data) => {
-                        setStartX(event.clientX);
-                        setStartY(event.clientY);
-                      }}
-                      onDragEnd={(event, data) => {
-                        if (isPlaced.every(item => item === 1)) {
-                          setEnd(true);
-                        }
-                        setStebs(stebs + 1);
-                        const div1Rect =
-                          divRef.current[
-                            event.target.id
-                          ].getBoundingClientRect();
-                        console.log(div1Rect.x);
-                        console.log(div1Rect.y);
-                        console.log(event.clientX - piece.width + 60);
-                        console.log(event.clientY);
-                        if (
-                          div1Rect.x < event.clientX - piece.width + 80 &&
-                          div1Rect.x > event.clientX - piece.width - 60
-                        ) {
-                          imgRef.current[event.target.id].style.position =
-                            'absolute';
-                          imgRef.current[event.target.id].style.left =
-                            div1Rect.left + 'px';
-                          imgRef.current[event.target.id].style.top =
-                            div1Rect.top + 'px';
-                          placed[event.target.id] = 1;
-                          const newState = [...isPlaced];
-                          newState[event.target.id] = 1;
-                          setIsPlaced(newState);
-                          // setIsPlaced([...isPlaced, placed[event.target.id]]);
-                        }
-                      }}
-                    />
-                  ))}{' '}
-                </div>
-              ))}
+        <>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '120px',
+
+          }}>
+            <div className='waviy'>
+              <span>Welcome</span>
+              <span>in</span>
+              <span>Puzzle</span>
+              <span>Game</span>
             </div>
-            <div>{divResive()}</div>
-          </>
-        )
+            <label for='inputTag'>
+              Select Image
+              <input id='inputTag' type='file' onChange={handleImageUpload} />
+            </label>
+          </div>
+
+          {/* <input type='file' onChange={handleImageUpload} /> <br /> */}
+          {image && (
+            <>
+              <img
+                src={URL.createObjectURL(image)}
+                alt='S'
+                onLoad={handleImageLoad}
+                id='main'
+              />{' '}
+              {/* <br />
+              <select id='pieces' value={pieces} onChange={handlePiecesChange}>
+                <option value={2}> 4 </option> <option value={4}> 16 </option>{' '}
+                <option value={9}> 81 </option>{' '}
+              </select>{' '}
+              <br /> */}
+              <h1 style={{ color: 'white' }}>{stebs}</h1>
+              <div>{divResive()}</div>
+              <div
+                style={{
+                  display: 'flex',
+                  width: '50%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  flexDirection: 'row-reverse',
+                  // position: 'relative',
+                }}
+              >
+                {rows.map(row => (
+                  <div
+                    style={{
+                      display: 'flex',
+                      width: '50%',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      flexDirection: 'row-reverse',
+                      // position: 'relative',
+                    }}
+                  >
+                    {row.map((piece, index) => (
+                      <img
+                        ref={el => (imgRef.current[piece.id] = el)}
+                        style={{
+                          position: 'realative',
+                        }}
+                        alt={`Piece ${piece.id}`}
+                        key={piece.id}
+                        id={piece.id}
+                        src={piece.dataUrl}
+                        onDragStart={(event, data) => {
+                          setStartX(event.clientX);
+                          setStartY(event.clientY);
+                        }}
+                        onDragEnd={(event, data) => {
+                          if (isPlaced.every(item => item === 1)) {
+                            setEnd(true);
+                          }
+                          setStebs(stebs + 1);
+                          const div1Rect =
+                            divRef.current[
+                              event.target.id
+                            ].getBoundingClientRect();
+                          console.log(div1Rect.x);
+                          console.log(div1Rect.y);
+                          console.log(event.clientX - piece.width + 60);
+                          console.log(event.clientY);
+                          if (
+                            div1Rect.x < event.clientX - piece.width + 80 &&
+                            div1Rect.x > event.clientX - piece.width - 60 &&
+                            div1Rect.y < event.clientY + 30 &&
+                            div1Rect.y > event.clientY - 30
+                          ) {
+                            imgRef.current[event.target.id].style.position =
+                              'absolute';
+                            imgRef.current[event.target.id].style.left =
+                              div1Rect.left + 'px';
+                            imgRef.current[event.target.id].style.top =
+                              div1Rect.top + 'px';
+                            placed[event.target.id] = 1;
+                            const newState = [...isPlaced];
+                            newState[event.target.id] = 1;
+                            setIsPlaced(newState);
+                            // setIsPlaced([...isPlaced, placed[event.target.id]]);
+                          }
+                        }}
+                      />
+                    ))}{' '}
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+        </>
       )}
     </div>
   );
